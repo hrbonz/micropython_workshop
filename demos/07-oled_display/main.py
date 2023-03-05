b7560a809bd115fde1950b9cb5683e65126a59fa
Stefan 'hr' Berder <stefan@measureofquality.com>
2023-03-05 20:23:46 +0800

    demo 7: flush display before using it

--- demos/07-oled_display/main.py
+++ demos/07-oled_display/main.py
@@ -9,6 +9,7 @@ from ssd1306 import SSD1306_I2C
 i2c = SoftI2C(sda=Pin(1), scl=Pin(0))
 oled = SSD1306_I2C(128, 64, i2c, addr=0x3c)
 
+oled.fill(0)
 oled.text("IoT workshop", 0, 0)
 oled.text("09/03/2023 18:30", 0, 20)
 oled.text("250 Tongren lu", 0, 30)
